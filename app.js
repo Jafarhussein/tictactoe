35c8b48a7f4a6b08292df22f3021960756515898
Jafarhussein <71325448+Jafarhussein@users.noreply.github.com>
2022-09-08 16:16:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,12 +23,17 @@ let running = true;
 initializeGame();
 
 function initializeGame(){
+//för varje click på en låda så ropar den på boxclicked function
 boxes.forEach(box=> box.addEventListener("click", boxClicked));
+//ropar på restartgame function
 restartBtn.addEventListener("click", restartGame);
+Ropar på currentplayer så att den skriver ut vilken av spelares tur det är
 statusText.textContent=`${currentPlayer}'s turn`;
 }
 function boxClicked(){
+//hämtar vår attribute av cell index från html
 const boxIndex = this.getAttribute("cellIndex")
+//om vår array inte har något i den och om spelet inte körs så skrivs inget ut
 if(options[boxIndex]!= "" || !running){
     return;
 }
@@ -46,6 +51,7 @@ function changePlayer(){
     statusText.textContent=`${currentPlayer}'s turn`;
 }
 function checkWinner(){
+// skapar boolean för att avsluta spelet när någon förlorar
     let roundWon =false;
     for(let i = 0; i<winConditions.length; i++){
         const condition = winConditions[i];
@@ -60,10 +66,13 @@ function checkWinner(){
             break;
         }
     }
+//om en spelare vinner då blir vår running boolean falskt och spelaren får en text
     if(roundWon){
         statusText.textContent=`${currentPlayer} Wins`;
         running=false;
-    }else if(!options.includes("")){
+    }
+//om det blir oavgjort så får man meddelandet och vår boolean blir falskt
+else if(!options.includes("")){
         statusText.textContent="Draw!";
         running=false
     }else{
